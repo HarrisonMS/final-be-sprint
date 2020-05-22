9f9681818e4a63f1b9470979bd7a0d32b5e6cbbc
Harrison <hmseaborn@gmail.com>
2020-05-22 09:34:23 -0700

    hotfix to generate token and added login
    generate token i was trying to intialize secret with secret.jwt instead of pointing towards a .env || string

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -32,16 +32,38 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcrypt.compareSync(password, user.password)) {
+          // produce and send a token that includes the username the user
+          const token = generateToken(user);
+
+          res.status(200).json({ message: "Welcome to our API", token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
 });
 
 function generateToken(user) {
   const payload = {
     subject: user.id,
     username: user.username,
-    role: user.role,
   };
-  const secret = secrets.jwtSecret;
+  const secret = process.env.JWT_SECRET || "tis a secret";
 
   const options = {
     expiresIn: "1d",
