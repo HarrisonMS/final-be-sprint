f8cb44bb1b27101ca5c2e0f00e787cc39d321a65
Harrison <hmseaborn@gmail.com>
2020-05-22 09:22:28 -0700

    endpoint /register functional
    returning just the hashed token could return token if imaginary client would have responded to me

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,30 @@ const Users = require("../users/model");
 const { isValid } = require("../users/services");
 
 router.post("/register", (req, res) => {
-  // implement registration
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash the password
+    const hash = bcrypt.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
 });
 
 router.post("/login", (req, res) => {
